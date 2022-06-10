d8eb5fe776203a9fa170b596e0070784461c8ee6
riasvdv <rias@spatie.be>
2022-06-10 14:40:53 +0000

    wip

--- tests/PostmarkTest.php
+++ tests/PostmarkTest.php
@@ -1,13 +1,17 @@
 <?php
 
 use Spatie\MailcoachPostmarkSetup\Postmark;
+use Spatie\MailcoachPostmarkSetup\Tests\TestCase;
 
-beforeEach(function() {
+uses(TestCase::class);
+
+beforeEach(function () {
     $this->postmark = new Postmark($this->token);
 });
 
-it('can validate the server token', function() {
+it('can validate the server token', function () {
     expect($this->postmark->hasValidServerToken())->toBeTrue();
 
-    expect(new Postmark('invalid-token'))->toBeFalse();
+    $hasValidToken = (new Postmark('invalid-token'))->hasValidServerToken();
+    expect($hasValidToken)->toBeFalse();
 });
